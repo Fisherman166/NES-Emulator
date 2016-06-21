ee3e9e74f64e9c1058e6d80131e75f9d3ed6c1b0
Sean <seankoppenhafer@gmail.com>
2016-06-20 21:45:36 -0700

    Decode logic unit test templates

---
+++ c_src/cpu_decode_logic_unit_tests.c
@@ -0,0 +1,26 @@
+//*****************************************************************************
+// Filename: cpu_decode_logic_unit_tests.c
+// Author: Fisherman166
+//
+// Implents all unit tests for cpu decode logic
+//
+// ****************************************************************************
+
+#include "cpu_decode_logic_unit_tests.h"
+#include "cpu_decode_logic.h"
+
+//*****************************************************************************
+// Helper function
+//*****************************************************************************
+static void my_print(char* string) {
+    printf("BASIC_CPU_DECODE_UT: %s", string);
+}
+
+//*****************************************************************************
+// Unit tests
+//*****************************************************************************
+void run_all_cpu_decode_logic_tests() {
+    my_print("Running all cpu decode logic tests\n");
+    my_print("Done testing all cpu decode logic tests\n");
+}
+
---
+++ c_src/cpu_decode_logic_unit_tests.h
@@ -0,0 +1,19 @@
+//*****************************************************************************
+// Filename: cpu_decode_logic_unit_tests.h
+// Author: Fisherman166
+//
+// Implements all unit tests for the cpu decode logic
+//
+// ****************************************************************************
+
+#ifndef CPU_DECODE_LOGIC_UNIT_TESTS
+#define CPU_DECODE_LOGIC_UNIT_TESTS
+
+#include <assert.h>
+#include <stdio.h>
+#include "common.h"
+
+void run_all_cpu_decode_logic_tests();
+
+#endif
+
