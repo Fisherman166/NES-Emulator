a731dc17c0ce719ca730a2e23558173c6f90ec6c
Sean <seankoppenhafer@gmail.com>
2017-04-16 19:56:00 -0700

    Move everything in main.c into functions

--- c_src/main.c
+++ c_src/main.c
@@ -14,57 +14,72 @@
 #include "sdl_interface.h"
 #include "ppu.h"
 
-void parse_cmdline(int, char **);
+static void parse_cmdline(int, char **);
+static void init_system(cpu_registers*);
+static void run_system(cpu_registers*);
+static void cleanup();
 
 static char* game_file;
 
 int main(int argc, char *argv[]) {
-    parse_cmdline(argc, argv);
-
-    #ifdef DEBUG
-        open_cpu_debug_logfile();
-    #endif
-
-    if( init_SDL() ) return 1;
-    bool game_loaded = load_game(game_file);
-    if(game_loaded) {
-        printf("FATAL ERROR occurred while attempting to load game.\n");
-        return -1;
-    }
     cpu_registers registers;
-    cold_boot_init(&registers);
 
-    printf("Entering main loop\n");
-    uint8_t cycles_executed;
-    bool nmi_flag = false;
-    uint32_t* pixel_data = get_pixel_data_ptr();
-    bool vblank;
-    bool old_vblank = false;
-
-    for(;;) {
-        nmi_flag = get_NMI_flag();
-        cycles_executed = execute_interpreter_cycle(&registers, nmi_flag);
-        for(uint8_t ppu_cycles = cycles_executed * 3; ppu_cycles > 0; ppu_cycles--)
-            vblank = run_PPU_cycle();
-        if( check_input(JOYPAD1) ) break;
-
-        if( vblank & !old_vblank ) render_frame(pixel_data);
-        old_vblank = vblank;
-    }
-    printf("Ending Emulation!\n");
-    exit_SDL();
-
-    #ifdef DEBUG
-        close_cpu_debug_logfile();
-    #endif
+    parse_cmdline(argc, argv);
+    init_system(&registers);
+    run_system(&registers);
+    cleanup();
 
     return 0;
 }
 
-void parse_cmdline(int argc, char **argv) {
+static void parse_cmdline(int argc, char **argv) {
     if(argc < 2) {
         printf("ERROR: No game path given. First argument should be game file path\n");
         exit(1);
     }
     game_file = argv[1];
 } 
+
+static void init_system(cpu_registers* registers) {
+    #ifdef DEBUG
+        open_cpu_debug_logfile();
+    #endif
+
+    if( init_SDL() ) {
+        printf("ERROR: Failed to init SDL");
+        exit(1);
+    }
+    if( load_game(game_file) ) {
+        printf("FATAL ERROR occurred while attempting to load game.\n");
+        exit(1);
+    }
+    cold_boot_init(registers);
+}
+
+static void run_system(cpu_registers* registers) {
+    uint8_t cycles_executed;
+    bool vblank;
+    bool old_vblank = false;
+    uint32_t* pixel_data = get_pixel_data_ptr();
+
+    printf("Entering main loop\n");
+    for(;;) {
+        cycles_executed = execute_interpreter_cycle(registers, get_NMI_flag());
+        for(uint8_t ppu_cycles = cycles_executed * 3; ppu_cycles > 0; ppu_cycles--) {
+            vblank = run_PPU_cycle();
+            if( vblank & !old_vblank ) render_frame(pixel_data);
+            old_vblank = vblank;
+        }
+        if( check_input(JOYPAD1) ) break;
+    }
+}
+
+static void cleanup() {
+    printf("Ending Emulation!\n");
+    exit_SDL();
+
+    #ifdef DEBUG
+        close_cpu_debug_logfile();
+    #endif
+}
+
