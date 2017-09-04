a7a52a4f03087032ea10b2823020b8853f52fa6d
Sean <seankoppenhafer@gmail.com>
2017-09-04 12:24:30 -0700

    Add LAX opcodes

--- c_src/cpu/illegal_opcodes.c
+++ c_src/cpu/illegal_opcodes.c
@@ -12,3 +12,47 @@ uint8_t absoluteX_NOP(cpu_registers* registers) {
     fetch_absoluteX(registers, &page_crossed);
     return page_crossed;
 }
+
+uint8_t zeropage_LAX(cpu_registers* registers) {
+    uint8_t data = fetch_zeropage(registers);
+    base_load_register(registers, &(registers->X), data);
+    base_load_register(registers, &(registers->A), data);
+    return ZERO_EXTRA_CYCLES;
+}
+
+uint8_t zeropageY_LAX(cpu_registers* registers) {
+    uint8_t data = fetch_zeropageY(registers);
+    base_load_register(registers, &(registers->X), data);
+    base_load_register(registers, &(registers->A), data);
+    return ZERO_EXTRA_CYCLES;
+}
+
+uint8_t absolute_LAX(cpu_registers* registers) {
+    uint8_t data = fetch_absolute(registers);
+    base_load_register(registers, &(registers->X), data);
+    base_load_register(registers, &(registers->A), data);
+    return ZERO_EXTRA_CYCLES;
+}
+
+uint8_t absoluteY_LAX(cpu_registers* registers) {
+    bool page_crossed;
+    uint8_t data = fetch_absoluteY(registers, &page_crossed);
+    base_load_register(registers, &(registers->X), data);
+    base_load_register(registers, &(registers->A), data);
+    return page_crossed;
+}
+
+uint8_t indirectX_LAX(cpu_registers* registers) {
+    uint8_t data = fetch_indirectX(registers);
+    base_load_register(registers, &(registers->X), data);
+    base_load_register(registers, &(registers->A), data);
+    return ZERO_EXTRA_CYCLES;
+}
+
+uint8_t indirectY_LAX(cpu_registers* registers) {
+    bool page_crossed;
+    uint8_t data = fetch_indirectY(registers, &page_crossed);
+    base_load_register(registers, &(registers->X), data);
+    base_load_register(registers, &(registers->A), data);
+    return page_crossed;
+}
--- c_src/cpu/illegal_opcodes.h
+++ c_src/cpu/illegal_opcodes.h
@@ -9,4 +9,11 @@ uint8_t zeropageX_NOP(cpu_registers*);
 uint8_t absolute_NOP(cpu_registers*);
 uint8_t absoluteX_NOP(cpu_registers*);
 
+uint8_t zeropage_LAX(cpu_registers*);
+uint8_t zeropageY_LAX(cpu_registers*);
+uint8_t absolute_LAX(cpu_registers*);
+uint8_t absoluteY_LAX(cpu_registers*);
+uint8_t indirectX_LAX(cpu_registers*);
+uint8_t indirectY_LAX(cpu_registers*);
+
 #endif
